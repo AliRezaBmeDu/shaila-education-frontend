dab44ee749e3b6cdbe6615050bb81dd53b374314
AliRezaBmeDu <ali.reza.buet@gmail.com>
2025-11-17 23:50:24 +0600

    Seventh

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -24,7 +24,7 @@ export default function RegisterPage() {
     }
 
     try {
-      const resUserExists = await fetch("/api/auth/register", {
+      const resUserExists = await fetch("/api/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
